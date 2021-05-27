63d452e4c26f738e3cf51c6bed146a21d5335b1e
Nathan <58734424+Nathiington@users.noreply.github.com>
2021-05-27 20:21:48 +0200

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import firebase from 'firebase/app'
 import 'firebase/auth'
 
 var firebaseConfig = {
-    apiKey: "AIzaSyCduuQTa3QPtSc9X_sxjtW-2LfINePC6T8",
-    authDomain: "logintemplate-71251.firebaseapp.com",
-    projectId: "logintemplate-71251",
-    storageBucket: "logintemplate-71251.appspot.com",
-    messagingSenderId: "358775363590",
-    appId: "1:358775363590:web:f8a10d49be17d4b1c64b87",
-    measurementId: "G-VL41X6TN11"
+    apiKey: "dummy value",
+    authDomain: "dummy value",
+    projectId: "dummy value",
+    storageBucket: "dummy value",
+    messagingSenderId: "dummy value",
+    appId: "dummy value",
+    measurementId: "dummy value"
 };
 // Initialize Firebase
 const app = firebase.initializeApp(firebaseConfig);
