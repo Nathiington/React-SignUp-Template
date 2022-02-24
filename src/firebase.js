b4b3f08fa699f8b3332569e333f50c6cfe0bf117
Nathan <58734424+Nathiington@users.noreply.github.com>
2022-02-24 13:02:58 +0200

    Added keys in environment variables

--- src/firebase.js
+++ src/firebase.js
@@ -1,18 +1,22 @@
 import firebase from 'firebase/compat/app'
 import 'firebase/compat/auth'
 
+const { REACT_APP_API_KEY, REACT_APP_AUTH_DOMAIN, REACT_APP_PROJECT_ID, REACT_APP_STORAGE_BUCKET, REACT_APP_MESSAGING_SENDER_ID, REACT_APP_APP_ID,REACT_APP_MEASUREMENT_ID } = process.env
+
 var firebaseConfig = {
-    apiKey: "dummy value",
-    authDomain: "dummy value",
-    projectId: "dummy value",
-    storageBucket: "dummy value",
-    messagingSenderId: "dummy value",
-    appId: "dummy value",
-    measurementId: "dummy value"
+    apiKey: REACT_APP_API_KEY,
+    authDomain: REACT_APP_AUTH_DOMAIN,
+    projectId: REACT_APP_PROJECT_ID,
+    storageBucket: REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: REACT_APP_MESSAGING_SENDER_ID,
+    appId: REACT_APP_APP_ID,
+    measurementId: REACT_APP_MEASUREMENT_ID
 };
 
 // Initialize Firebase
 const app = firebase.initializeApp(firebaseConfig);
 export const auth = app.auth()
 export {app}
+
+console.log( REACT_APP_API_KEY)
 
